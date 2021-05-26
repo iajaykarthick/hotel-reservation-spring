3a20895620df4ad05e6d15866eba728e801f8a9d
Ajay Karthick <24907339+iajaykarthick@users.noreply.github.com>
2021-05-26 14:03:00 +0530

    First Commit

---
+++ src/main/java/com/ak/learning/data/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.ak.learning.data.repository;
+
+import com.ak.learning.data.entity.Reservation;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+}
---
+++ src/main/java/com/ak/learning/data/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.ak.learning.data.repository;
+
+import com.ak.learning.data.entity.Room;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long> {
+
+}
